54940d401eb43b5ab8e514beb6475ddef729c442
Marquise Piton <pitonmarquise@gmail.com>
2021-05-02 12:02:55 -0400

    made modifications to register component

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,22 +1,68 @@
-import React,{useState} from 'react';
-import { Switch } from "react-router";
+import React, { useState, useEffect } from "react";
 import "./Register.css";
+import axios from "axios";
 
-function Register(props) {
-  const [formData, setFormData] = useState({})
-
+function Register() {
+// ================================ useState===================================================
+  const [formData, setFormData] = useState({});
+  const [teamData, setTeamData] = useState([]);
+  const [playerData, setPlayerData] = useState([]);
+  //================================ Axios calls ==============================================
   const handleForm = (e) => {
-
-    setFormData(previousState => (
-      {
-        ...previousState,
-        [e.target.name]: e.target.value
-      }
-    ))
-
-    
-
-  }
+    setFormData((previousState) => ({
+      ...previousState,
+      [e.target.name]: e.target.value,
+    }));
+  };
+  console.log(formData);
+  const handleSubmit = (e) => {
+    const apiUrl =
+      "https://goat-pitonmarquise819185.codeanyapp.com/api/register";
+    e.preventDefault();
+    axios.post(apiUrl, formData)
+    .then(response => {
+      console.log(response)
+    })
+    .catch(error => {
+      console.log(error);
+    })
+  };
+  // Extract team data
+  const axiosGetTeam = () => {
+    const apiUrl =
+      "https://goat-pitonmarquise819185.codeanyapp.com/api/team/all";
+    axios.get(apiUrl).then(function (response) {
+      setTeamData(response.data);
+    });
+  };
+ // Extract player data
+  const axiosGetPlayer = () => {
+    const apiUrl =
+      "https://goat-pitonmarquise819185.codeanyapp.com/api/player/all";
+    axios.get(apiUrl).then(function (response) {
+      setPlayerData(response.data);
+    });
+  };
+  // ================================ useEffect==================================================
+  useEffect(axiosGetPlayer, []);
+  useEffect(axiosGetTeam, []);
+ // ====================================== MAPPING============================================== 
+  // Map the team data into the scroll component
+ const teams = teamData.map((data, index) => {
+    return (
+      <option  key={index} value={data.teamname}>
+        {data.teamname}
+      </option>
+    );
+  })
+  // Map the player data into the scroll component
+  const mapPlayers = playerData.map((data, index) => {
+    return (
+      <option key={index} value={data.name}>
+        {data.name}
+      </option>
+    );
+  });
   return (
     <div className="container">
       <div className="register">
@@ -25,11 +71,11 @@ function Register(props) {
             <h1>Register!</h1>
           </div>
           <div className="card-body">
-            <form>
+            <form onSubmit={handleSubmit}>
               <div className="form-outline mb-4">
                 <input
                   type="text"
-                  name='username'
+                  name="username"
                   id="registerUsername"
                   className="form-control"
                   onChange={handleForm}
@@ -64,7 +110,7 @@ function Register(props) {
                     Password
                   </label>
                 </div>
-                <div className="form-outline mb-4">
+                {/* <div className="form-outline mb-4">
                   <input
                     type="password"
                     name="repeatPassword"
@@ -76,6 +122,36 @@ function Register(props) {
                   <label className="form-label" for="registerRepeatPassword">
                     Repeat password
                   </label>
+                </div> */}
+        
+
+                <div className="form-outline mb-4">
+                  <div className="col-12">
+                    <label
+                      className="visually-hidden"
+                      for="inlineFormSelectPref"
+                      
+                    >
+                      Fan Of
+                    </label>
+                    <select 
+                     onChange={handleForm} name="fanof"
+                    className="select">{mapPlayers}</select>
+                  </div>
+                </div>
+
+                <div className="form-outline mb-4">
+                  <div className="col-12">
+                    <label
+                      className="visually-hidden"
+                      for="inlineFormSelectPref"
+                    >
+                      Team
+                    </label>
+                    <select 
+                     onChange={handleForm} name="favoriteteam"
+                    className="select">{teams}</select>
+                  </div>
                 </div>
 
                 <button
